fb649b4811b113d9982910b5714daa7de28194f8
Caio Queiroz <caio.softbuilder>
2021-10-15 21:35:05 -0300

    add dotenv

--- src/app/http/server.ts
+++ src/app/http/server.ts
@@ -1,3 +1,4 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
 import {ExceptionHandlerGlobal} from './middlewares/ErrorsMiddleware';
 import {ChangeHeaderXPoweredXMiddleware} from './middlewares/ChangeHeaderXPowered';
@@ -5,6 +6,8 @@ import 'express-async-errors';
 import {routes} from '../routes';
 import cors from 'cors';
 import '../container';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -16,4 +19,4 @@ app.use(ExceptionHandlerGlobal);
 
 app.use(ChangeHeaderXPoweredXMiddleware);
 
-app.listen(3333, () => console.log('server rodando, porta 3333'));
+app.listen(process.env.APP_PORT || 3334, () => console.log(`server rodando, porta ${process.env.APP_PORT}`));
